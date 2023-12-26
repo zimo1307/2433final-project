47d958020c638aa3e497c3ca58a6a356c6aeb93b
zimo1307 <145531134+zimo1307@users.noreply.github.com>
2023-12-26 12:01:08 +0800

    Delete final_project/end2endapp/config.py

--- final_project/end2endapp/config.py
+++
@@ -1,7 +0,0 @@
-import os
-import yaml
-from easydict import EasyDict
-
-def get_config_dict(input_file_path="C:\\zqhome\\NYU Course\\DBS\\FinalProject\\code\\config.yaml"):
-    with open(input_file_path, 'r') as stream:
-        return EasyDict(yaml.safe_load(stream))
